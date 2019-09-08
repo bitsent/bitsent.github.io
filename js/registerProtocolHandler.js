3b01715c9f7fcd851ff45499602f6ac68a4786f6
Siko91 <alexander.n.dinkov@gmail.com>
2019-09-08 20:27:26 +0300

    registerProtocolHandler fix

--- js/registerProtocolHandler.js
+++ js/registerProtocolHandler.js
@@ -1,7 +1,7 @@
 console.log("registerProtocolHandler - start");
 navigator.registerProtocolHandler(
   "bitcoin-req",
-  "https://bitsent.net/bip275.html",
+  "https://bitsent.net/bip275.html?data=%s",
   "BitSent BIP-275"
 );
 console.log("registerProtocolHandler - done");
