a245cbef7d1ac904b6acb17ae9dc49aeef19e699
Aleksandar Nikolaev Dinkov <and@milestone.dk>
2019-09-10 12:49:51 +0300

    bitcoin uri scheme only

--- js/registerProtocolHandler.js
+++ js/registerProtocolHandler.js
@@ -1,20 +1,9 @@
 try {
-    navigator.registerProtocolHandler(
-      "bitcoin-req",
-      "https://bitsent.net/bip275.html?req=%s",
-      "BitSent"
-    );
-  } catch (error) {
-    console.log("'bitcoin-req' is not a whitelisted URI scheme", error);
-  }
-
-  try {
-    navigator.registerProtocolHandler(
-      "bitcoin",
-      "https://bitsent.net/bitcoin-uri.html?req=%s",
-      "BitSent"
-    );
-  } catch (error) {
-    console.error(error);
-  }
-    
+  navigator.registerProtocolHandler(
+    "bitcoin",
+    "https://bitsent.net/bitcoin-uri.html?req=%s",
+    "BitSent"
+  );
+} catch (error) {
+  console.error(error);
+}
